637d491446e3e5ce1a3e58a14b4327f5197aab49
缺小小 <qtxcxc@gmail.com>
2019-12-14 00:07:39 +0800

    feat: 添加外部模型和点击事件

--- src/app/application.ts
+++ src/app/application.ts
@@ -1,46 +1,34 @@
 import Stats from 'stats.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, TextureLoader, DoubleSide } from 'three';
+import { AnimationAction } from 'three/src/animation/AnimationAction';
+import { Scene, PerspectiveCamera, WebGLRenderer, PlaneBufferGeometry, Mesh, MeshBasicMaterial, DoubleSide, GridHelper, AmbientLight, AnimationMixer, Clock, Raycaster, Vector2, Object3D } from 'three';
 
 export class Application {
   private scene: Scene;
+  private clock: Clock;
   private camera: PerspectiveCamera;
   private renderer: WebGLRenderer;
   private stats: Stats;
+  private gltfLoader: GLTFLoader;
+  private walkAction: AnimationAction;
+  private animationMixer: AnimationMixer;
+  // 动画是否暂停
+  private paused: boolean = false;
 
   constructor() {
     this.scene = new Scene();
+    this.clock = new Clock();
     this.camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     this.renderer = new WebGLRenderer({ antialias: true });
+    this.gltfLoader = new GLTFLoader();
+    this.animationMixer = new AnimationMixer(this.scene);
+
     this.renderer.setSize(window.innerWidth, window.innerHeight);
     document.body.appendChild(this.renderer.domElement);
     window.addEventListener('resize', () => this.onWindowResize());
 
-    const textureLoader = new TextureLoader();
-
-    const boxTexture = textureLoader.load('./assets/box.jpg');
-
-    const boxGeometry = new BoxGeometry(1, 1, 1);
-    const meshBasicMaterial = new MeshBasicMaterial({ map: boxTexture, side: DoubleSide });
-    const mesh = new Mesh(boxGeometry, meshBasicMaterial);
-    mesh.name = 'box';
-
-    const skyboxGeometry = new BoxGeometry(200, 200, 200);
-    const skyboxMaterials = [
-      new MeshBasicMaterial({ map: textureLoader.load('./assets/skybox/rt.png'), side: DoubleSide }),
-      new MeshBasicMaterial({ map: textureLoader.load('./assets/skybox/lf.png'), side: DoubleSide }),
-      new MeshBasicMaterial({ map: textureLoader.load('./assets/skybox/up.png'), side: DoubleSide }),
-      new MeshBasicMaterial({ map: textureLoader.load('./assets/skybox/dn.png'), side: DoubleSide }),
-      new MeshBasicMaterial({ map: textureLoader.load('./assets/skybox/bk.png'), side: DoubleSide }),
-      new MeshBasicMaterial({ map: textureLoader.load('./assets/skybox/ft.png'), side: DoubleSide }),
-    ];
-    const skyboxMesh = new Mesh(skyboxGeometry, skyboxMaterials);
-    skyboxMesh.name = 'skyboxMesh';
-
-    this.camera.position.set(0, 0, 5);
-
-    this.scene.add(mesh);
-    this.scene.add(skyboxMesh);
+    this.camera.position.set(0, 5, 5);
 
     this.stats = new Stats();
     this.stats.showPanel(0);
@@ -48,18 +36,69 @@ export class Application {
 
     new OrbitControls(this.camera, this.renderer.domElement);
 
+    const planeBufferGeometry = new PlaneBufferGeometry(100, 100);
+    const plane = new Mesh(planeBufferGeometry, new MeshBasicMaterial({ color: 0xFFFFFF, side: DoubleSide }));
+    plane.name = 'plane';
+    plane.rotation.x = -Math.PI / 2;
+    this.scene.add(plane);
+    this.scene.add(new GridHelper(100, 100));
+
+    this.gltfLoader.load('../assets/Soldier.glb', gltf => {
+      console.log(gltf);
+      gltf.scene.name = 'Soldier';
+      this.scene.add(gltf.scene);
+      this.scene.add(new AmbientLight(0xFFFFFF, 2));
+
+      const animationClip = gltf.animations.find(animationClip => animationClip.name === 'Walk');
+      this.walkAction = this.animationMixer.clipAction(animationClip);
+      this.walkAction.play();
+    });
+
+    this.renderer.domElement.addEventListener('click', event => {
+      const { offsetX, offsetY }  = event;
+      const x = ( offsetX / window.innerWidth ) * 2 - 1;
+    	const y = - ( offsetY / window.innerHeight ) * 2 + 1;
+      const mousePoint = new Vector2(x, y);
+      const raycaster = new Raycaster();
+      // 设置鼠标位置和参考相机
+      raycaster.setFromCamera(mousePoint, this.camera);
+      // 鼠标点击对应的物体（所有鼠标映射到的物体，包括被遮挡的）
+      const intersects =  raycaster.intersectObjects(this.scene.children, true);
+      // 过滤网格和地面
+      const intersect = intersects.filter(intersect => !(intersect.object instanceof GridHelper) && intersect.object.name !== 'plane')[0];
+      if(intersect && this.isClickSoldier(intersect.object)) {
+        // 停止动画
+        // this.walkAction.stop();
+        // 暂停动画
+        this.walkAction.paused = !this.walkAction.paused;
+      }
+    });
+
     this.render();
   }
 
-  private render() {
-    this.stats.begin();
-    window.requestAnimationFrame(() => this.render());
-    const skyboxMesh = this.scene.getObjectByName('skyboxMesh');
+  /**
+   * 递归判断是否点击到人物
+   * @param object
+   */
+  private isClickSoldier(object: Object3D) {
+    if(object.name === 'Soldier') {
+      return object;
+    } else if(object.parent) {
+      return this.isClickSoldier(object.parent);
+    } else {
+      return null;
+    }
+  }
 
-    skyboxMesh.rotation.y += 0.001;
+  private render() {
+    // 更新动画
+    this.animationMixer.update(this.clock.getDelta());
 
     this.renderer.render(this.scene, this.camera);
-    this.stats.end();
+
+    window.requestAnimationFrame(() => this.render());
+    this.stats.update();
   }
 
   private onWindowResize() {
